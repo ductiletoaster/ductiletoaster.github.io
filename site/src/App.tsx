c111c2fd66c8b12be223cf98674d859915da18b3
Brian Gebel <bgebel@gofundme.com>
2020-08-20 08:38:29 -0700

    fix: adding div timeline

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -60,30 +60,37 @@ function App() {
                     </div>
                     <div className="information column">
                         <h4>GoFundMe</h4>
-                        <h5>Senior Software Engineer, Manager</h5>
-                        <h6 className="subtitle">Aug. 2019-Present</h6>
-                        <h6 className="subtitle">San Diego, CA</h6>
-                        <br />
-                        <h5>Senior Software Engineer</h5>
-                        <h6 className="subtitle">Oct. 2015-Aug. 2019</h6>
-                        <h6 className="subtitle">San Diego, CA</h6>
-                        <br />
+                        <div className="timeline">
+                            <h5>Senior Software Engineer, Manager</h5>
+                            <h6 className="subtitle">Aug. 2019-Present</h6>
+                            <h6 className="subtitle">San Diego, CA</h6>
+                            <br />
+                        </div>
+                        <div className="timeline">
+                            <h5>Senior Software Engineer</h5>
+                            <h6 className="subtitle">Oct. 2015-Aug. 2019</h6>
+                            <h6 className="subtitle">San Diego, CA</h6>
+                            <br />
+                        </div>
                         <p>Currently part of a rapidly growing team of Software Engineers, QA Analysts, and product specialists tasked with growing the GoFundMe platform.</p>
                         <ul>
                             <li>Responsible for building and maintaining numerous customer service and business development tools that integrate heavily into our user facing product.</li>
                             <li>Worked as part of a team to build an image review tool that handles approximately 18 thousands images a day.</li>
                             <li>Integrated with a third party service to do advanced keyword analysis with the goal of protecting our visitors from malicious users.</li>
                         </ul>
-
-                        <h4>Conversion Voodoo</h4>
-                        <h5>Senior Web Developer</h5>
-                        <h6 className="subtitle">Oct. 2014-Oct. 2015</h6>
-                        <h6 className="subtitle">San Diego, CA</h6>
-                        <br />
-                        <h5>Web Developer</h5>
-                        <h6 className="subtitle">Jan. 2011-Oct. 2015</h6>
-                        <h6 className="subtitle">San Diego, CA</h6>
-                        <br />
+                        <div className="timeline">
+                            <h4>Conversion Voodoo</h4>
+                            <h5>Senior Web Developer</h5>
+                            <h6 className="subtitle">Oct. 2014-Oct. 2015</h6>
+                            <h6 className="subtitle">San Diego, CA</h6>
+                            <br />
+                        </div>
+                        <div className="timeline">
+                            <h5>Web Developer</h5>
+                            <h6 className="subtitle">Jan. 2011-Oct. 2015</h6>
+                            <h6 className="subtitle">San Diego, CA</h6>
+                            <br />
+                        </div>
                         <p>Worked with a small team of developers and designers, using advanced development strategies, to implement and maintain custom reporting and testing systems for numerous clients.</p>
                         <ul>
                             <li>Maintained a multitude of custom web applications and systems for an ever changing client base.</li>
@@ -92,11 +99,13 @@ function App() {
                             <li>Implement front-end AB and multivariate tests to improve site performance. Many of our clients saw a 15% - 150% improvement in conversion rate. Resulting in millions of dollars in additional revenue.</li>
                         </ul>
 
-                        <h4>San Diego Luxury Transportation</h4>
-                        <h5>Web Designer/IT Administrator</h5>
-                        <h6 className="subtitle">Oct. 2009-Jan. 2011</h6>
-                        <h6 className="subtitle">San Diego, CA</h6>
-                        <br />
+                        <div className="timeline">
+                            <h4>San Diego Luxury Transportation</h4>
+                            <h5>Web Designer/IT Administrator</h5>
+                            <h6 className="subtitle">Oct. 2009-Jan. 2011</h6>
+                            <h6 className="subtitle">San Diego, CA</h6>
+                            <br />
+                        </div>
                         <p>Created web sites geared toward specific regional ad campaigns, implemented new infrastructure technologies and provided administrative support for company systems.</p>
                         <ul>
                             <li>Designed and implemented websites for various marketing campaigns including PPC and affiliate traffic.</li>
