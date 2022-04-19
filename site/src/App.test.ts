b3cccd1de257a67d075245dcac6cb1258f026562
Brian Gebel <bgebel@gofundme.com>
2022-04-18 18:27:38 -0700

    upgrade create react version

--- site/src/App.test.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders Brian Gebel', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Brian Gebel/i);
-  expect(linkElement).toBeInTheDocument();
-});
